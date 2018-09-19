64ae0d18947ede19d961adfa0cb5e4a67f6c2f8d
Michael Berlian <Michael@Michaels-MacBook-Pro-2.local>
2018-09-19 13:28:49 +0700

    Better Stars

--- stars1.py
+++ stars1.py
@@ -36,7 +36,7 @@ for i in range (1,number+1):
 print()
 
 for i in range (1,number+1):
-    for p in range (1,2*number):
+    for p in range (1,number+i+1):
         if ((p<number-i+1) or (p>number+i-1)):
             print(' ',end="")
         else:
@@ -44,7 +44,7 @@ for i in range (1,number+1):
     print()
 for i in range (2,number+1):
     print (end=" ")
-    for p in range (2,2*number-1):
+    for p in range (2,2*number-i+1):
         if ((p>=i) and (p<=(2*number-i))):
             print('*',end="")
         else:
@@ -53,7 +53,7 @@ for i in range (2,number+1):
 print()
 
 for i in range (1,number+1):
-    for p in range (1,2*number):
+    for p in range (1,2*number+i+1):
         if ((p!=number-i+1) and (p!=number+i-1)):
             print(' ',end="")
         else:
@@ -61,7 +61,7 @@ for i in range (1,number+1):
     print()
 for i in range (2,number+1):
     print (end=" ")
-    for p in range (2,2*number-1):
+    for p in range (2,2*number-i+1):
         if ((p==i) or (p==(2*number-i))):
             print('*',end="")
         else:
