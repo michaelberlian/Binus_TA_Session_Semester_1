db0d919bdeecfb457732ad1416f4f9ce35a7be2d
Michael Berlian <Michael@Michaels-MacBook-Pro-2.local>
2018-09-19 11:39:20 +0700

    Better Stars

---
+++ stars1.py
@@ -0,0 +1,53 @@
+number= int(input("Input your number : "))
+
+for i in range (1,number+1):
+    for p in range (1,i+1):
+        print("*",end="")
+    print()
+
+for i in range (1,number+1):
+    for p in range (1,number+1):
+        if ((number-i)>=p):
+            print(' ',end="")
+        else:
+            print('*',end="")
+    print()
+
+for i in range (1,number+1):
+    for p in range (1,number-i+2):
+        print('*',end="")
+    print()
+
+for i in range (1,number+1):
+    for p in range (1,number+1):
+        if (i>p):
+            print (' ',end="")
+        else:
+            print ('*',end="")
+    print()
+
+for i in range (1,number+1):
+    for p in range (1,2*number):
+        if ((p<number-i+1) or (p>number+i-1)):
+            print(' ',end="")
+        else:
+            print('*',end="")
+    print()
+
+
+for i in range (1,number+1):
+    for p in range (1,2*number):
+        if ((p<number-i+1) or (p>number+i-1)):
+            print(' ',end="")
+        else:
+            print('*',end="")
+    print()
+for i in range (2,number+1):
+    print (end=" ")
+    for p in range (2,2*number-1):
+        if ((p>=i) and (p<=(2*number-i))):
+            print('*',end="")
+        else:
+            print(' ',end="")
+    print()
+
