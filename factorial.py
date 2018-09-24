617734b59baafe5154f7e01391dda2b6a22d587e
Michael Berlian <Michael@Michaels-MacBook-Pro-2.local>
2018-09-24 15:51:42 +0700

    Factorial

---
+++ factorial.py
@@ -0,0 +1,5 @@
+number = int(input("Input the number: "))
+count=1
+for i in range (1,number+1):
+    count = count * i
+print (count)
---
+++ odd-even.py
@@ -0,0 +1,3 @@
+number=int(input())
+ans =["even","odd"]*number
+print (ans[number])
